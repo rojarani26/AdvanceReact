29b2f317d90235e9a8776a1bc15a975ff0862cbc
rojarani26 <rojarani0215@gmail.com>
2023-12-15 14:34:26 +0530

    Task 9

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import ExpenseItem from './Components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      locationOfExpenditure: 'Bathroom',
+    },
+    { 
+      id: 'e2', 
+      title: 'New TV', 
+      amount: 799.49, 
+      date: new Date(2021, 2, 12), 
+      locationOfExpenditure: 'Living Room',
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      locationOfExpenditure: 'Insurance',
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      locationOfExpenditure: 'Office room',
+    },
+  ];
+
   return (
     <div>
-      <ExpenseItem></ExpenseItem>
-    </div>
+    <h2>Let's get started!</h2>
+    {expenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id} 
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+        locationOfExpenditure={expense.locationOfExpenditure}
+      />
+    ))}
+  </div>
   );
 }
 
 export default App;
+
