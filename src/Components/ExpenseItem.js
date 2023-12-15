6e7a09f83f7f7c4f9a6c5bb8a3e3d47ee2f45354
rojarani26 <rojarani0215@gmail.com>
2023-12-15 12:54:36 +0530

    Task 7

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,13 +1,15 @@
-function ExpenseItem(){
-    return(
-  <div>
-    <h2>Expense Items</h2>
-    <h3>Food Rs 10</h3>
-    <h3>Petrol Rs 100</h3>
-    <h3>Movies Rs 200</h3>
-  </div>
-  
-    );
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
